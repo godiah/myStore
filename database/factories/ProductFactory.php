f36bf8a3242420604b54b176ae4cce99f9e75b69
lewys-miugo <lewiswambugu01@gmail.com>
2023-07-19 15:23:28 +0300

    Building product logic

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\odel=Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $product_name=$this->faker->unique()->words($nb=3,$asText=true);
+        $slug = Str::slug($product_name,'-');
+        return [
+            //
+            'name'=>$product_name,
+            'slug'=>$slug,
+            'description'=>$this->faker->text(200),
+            'regular_price'=> $this->faker->numberBetween(5000,100000),
+            'stock_status'=>'instock',
+            'quantity'=>$this->faker->numberBetween(10,50),
+            'image'=>'product-'.$this->faker->numberBetween(1,5),
+            'category_id'=>$this->faker->numberBetween(1,5)
+
+        ];
+    }
+}
