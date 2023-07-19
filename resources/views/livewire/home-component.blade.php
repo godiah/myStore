297719074948bacb85fc60eb6c17b582228192f7
lewys-miugo <lewiswambugu01@gmail.com>
2023-07-19 08:15:20 +0300

    staging livewire

---
+++ resources/views/livewire/home-component.blade.php
@@ -0,0 +1,103 @@
+<div>
+    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+    <main>
+    <!-- Hero Section -->
+    <div class="main-hero" id="hero">
+      <div class="horizon-line">
+        <h3>Welcome to My Store</h3>
+      </div>
+      <p class="heading"><h1>Discounted Prices : Wholesale & Retail</h1></p>
+      <div class="horizon-line">
+        <p>Save time and money on free deliveries on orders above Ksh. 10,000</p>
+      </div>
+      <div class="btn-box">
+        <a href="shop_computing.html" class="btn1">Shop Computers</a>
+        <a href="shop_networking.html" class="btn1">Shop Networking</a>
+        <a href="shop_page.html" class="btn1">Shop All</a>
+      </div>
+    </div>
+
+    <!-- Browse Our Categories -->
+    <section class="category_layout">
+      <div class="heading_center container">
+        <h2><span>Our Categories</span></h2>
+      </div>
+    </section>
+    <section id="categories-card">
+      <div class="category-container">
+        <div class="category-cards">
+          <div class="category-card">
+            <a href="shop_accessories.html"><img src="images/product2.png" alt="Product Image"></a>
+            <h5><b>Accesories</b></h5>            
+          </div>      
+          <div class="category-card">
+            <a href="shop_computing.html"><img src="images/hp2.png" alt="Product Image"></a>            
+            <h5><b>Computing</b></h5>            
+          </div>      
+          <div class="category-card">
+            <a href="shop_electronics.html"><img src="images/product3.png" alt="Product Image"></a>            
+            <h5><b>Electronics</b></h5>
+          </div>
+         <div class="category-card">
+            <a href="shop_networking.html"><img src="images/product1.jpg" alt="Product Image"></a>
+            <h5><b>Networking</b></h5>
+          </div>
+        <!-- Add more category cards here -->
+        </div>                    
+      </div>
+    </section>
+
+    <!-- Best Selling Products -->
+    <section class="category_layout">
+      <div class="heading_center container">                
+        <h2><span>Best Selling Products</span></h2>
+      </div>
+    </section>
+    <section class="best-selling-product"> 
+      <button class="pre-btn"><img src="images/arrow.png" alt=""></button>
+      <button class="nxt-btn"><img src="images/arrow.png" alt=""></button>
+      <div class="best-selling-product-container">
+        <div class="best-selling-product-card">
+          <div class="best-selling-product-image">
+            <img src="images/product1.jpg" class="product-thumb" alt="">
+          </div>
+          <div class="best-selling-product-info">
+            <h2 class="best-selling-product-brand">UGREEN Bluetooth 5.3 USB Adapter CM591</h2>
+            <p class="product-short-description"><a href="product_details.html">ASUS E410MA-BV1517, Intel Celeron N4020, 4GB DDR4 2666, 256GB SSD</a></p>
+            <span class="best-selling-price">Ksh999.99</span>
+          </div>
+        </div>
+        <div class="best-selling-product-card">
+          <div class="best-selling-product-image">
+            <img src="images/hp.webp" class="product-thumb" alt="">
+          </div>
+          <div class="best-selling-product-info">
+            <h2 class="best-selling-product-brand">UGREEN Bluetooth 5.3 USB Adapter CM591</h2>
+            <p class="product-short-description"><a href="product_details.html">ASUS E410MA-BV1517, Intel Celeron N4020, 4GB DDR4 2666, 256GB SSD</a></p>
+            <span class="best-selling-price">Ksh999.99</span>
+          </div>
+        </div>
+        <div class="best-selling-product-card">
+          <div class="best-selling-product-image">
+            <img src="images/hp2.png" class="product-thumb" alt="">
+          </div>
+          <div class="best-selling-product-info">
+            <h2 class="best-selling-product-brand">UGREEN Bluetooth 5.3 USB Adapter CM591</h2>
+            <p class="product-short-description"><a href="product_details.html">ASUS E410MA-BV1517, Intel Celeron N4020, 4GB DDR4 2666, 256GB SSD</a></p>
+            <span class="best-selling-price">Ksh999.99</span>
+          </div>
+        </div>
+        <div class="best-selling-product-card">
+          <div class="best-selling-product-image">
+            <img src="images/product1.jpg" class="product-thumb" alt="">
+          </div>
+          <div class="best-selling-product-info">
+            <h2 class="best-selling-product-brand">UGREEN Bluetooth 5.3 USB Adapter CM591</h2>
+            <p class="product-short-description"><a href="product_details.html">ASUS E410MA-BV1517, Intel Celeron N4020, 4GB DDR4 2666, 256GB SSD</a></p>
+            <span class="best-selling-price">Ksh999.99</span>
+          </div>
+        </div>  
+      </div>
+    </section>
+  </main>
+</div>
