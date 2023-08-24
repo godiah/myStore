d6c92ff25fd579e4cf4faa91650122e254e96a8c
lewys-miugo <lewiswambugu01@gmail.com>
2023-08-24 16:44:11 +0300

    Adding the Add to Cart functionality

--- app/Http/Livewire/DetailsComponent.php
+++ app/Http/Livewire/DetailsComponent.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Product;
+use Cart;
+
 
 class DetailsComponent extends Component
 {
@@ -12,6 +14,14 @@ class DetailsComponent extends Component
     public function mount($slug){
         $this->slug = $slug;
     }
+
+    public function store($product_id,$product_name,$product_price)
+    {
+        Cart::add($product_id,$product_name,1,$product_price)->associate('\App\Models\Product');
+        session()->flash('success_message','Item added to cart');
+        return redirect()->route('shop.cart');
+    }
+
     public function render()
     {
         $product = Product::where('slug',$this->slug)->first();
