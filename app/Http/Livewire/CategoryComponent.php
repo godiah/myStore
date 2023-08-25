134357206c1c814366362a5456b1e6cf11377262
lewys-miugo <lewiswambugu01@gmail.com>
2023-08-25 12:56:14 +0300

    Creating the category functionality

--- app/Http/Livewire/CategoryComponent.php
+++ app/Http/Livewire/CategoryComponent.php
@@ -14,6 +14,8 @@ class CategoryComponent extends Component
 {
     use WithPagination;
     public $slug;
+    public $pageSize=10;
+    public $orderBy="Default Sorting";
 
     public function store($product_id,$product_name,$product_price)
     {
@@ -27,13 +29,44 @@ class CategoryComponent extends Component
         $this->slug=$slug;
     }
 
+    public function changePageSize($size)
+    {
+        $this->pageSize=$size;
+    }
+
+    public function changeOrderBy($order)
+    {
+        $this->orderBy = $order;
+    }
+
+
     public function render()
     {
+        
         $category=Category::where('slug',$this->slug)->first();
         $category_id=$category->id;
         $category_name=$category->name;
 
-        $products=Product::where('category_id',$category_id)->paginate(10);
+        // $products=Product::where('category_id',$category_id)->paginate(10);
+
+        if ($this->orderBy == 'Price: Low to High') 
+        {
+            
+            $products=Product::where('category_id',$category_id)->orderBy('regular_price','ASC')->paginate($this->pageSize);
+            
+        } 
+        elseif ($this->orderBy == 'Price: High to Low') 
+        {
+            
+            $products=Product::where('category_id',$category_id)->orderBy('regular_price','DESC')->paginate($this->pageSize);
+        } 
+        elseif ($this->orderBy == 'Latest') {
+            $products=Product::where('category_id',$category_id)->orderBy('created_at','DESC')->paginate($this->pageSize);
+        } 
+        else {
+            
+            $products=Product::where('category_id',$category_id)->paginate($this->pageSize);
+        }
 
 
         $categories=Category::orderBy('name','ASC')->get();
--- resources/views/livewire/category-component.blade.php
+++ resources/views/livewire/category-component.blade.php
@@ -51,10 +51,124 @@
         </div>
         <!-- Products -->
         <div class="col-md-9"> 
-          <div>
-            <p style="text-decoration: none;">We have found <strong>{{$products->total()}}</strong> products from <strong>{{$category_name}}</strong> for you.</p>
-          </div>               
-          <div class="shop-container">
+            <div>
+                <p style="text-decoration: none;">We have found <strong>{{$products->total()}}</strong> products from <strong>{{$category_name}}</strong> for you.</p>
+            </div>        
+            
+            <!-- sort by page size -->
+            <style>
+                    /* Style the dropdown container */
+                    .dropdown {
+                    position: relative;
+                    display: inline-block;
+                    }
+
+                    /* Style the button that toggles the dropdown */
+                    .dropbtn {
+                    background-color: #3498db;
+                    color: white;
+                    padding: 10px;
+                    border: none;
+                    cursor: pointer;
+                    }
+
+                    /* Style the dropdown content (hidden by default) */
+                    .dropdown-content {
+                    display: none;
+                    position: absolute;
+                    background-color: #f9f9f9;
+                    min-width: 160px;
+                    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+                    z-index: 1;
+                    }
+
+                    /* Style the dropdown content links */
+                    .dropdown-content a {
+                    color: black;
+                    padding: 12px 16px;
+                    text-decoration: none;
+                    display: block;
+                    }
+
+                    /* Change color of dropdown content links on hover */
+                    .dropdown-content a:hover {
+                    background-color: #ddd;
+                    }
+
+                    /* Show the dropdown content when hovering over the container */
+                    .dropdown:hover .dropdown-content {
+                    display: block;
+                    }
+
+            </style>
+
+            <div class="dropdown">
+                <button class="dropbtn" >Show: {{$pageSize}} products</button>
+                <ul class="dropdown-content">
+                    <li><a href="#" wire:click.prevent="changePageSize(10)">10 Products</a></li>
+                    <li><a href="#" wire:click.prevent="changePageSize(15)">15 Products</a></li>
+                    <li><a href="#" wire:click.prevent="changePageSize(20)">20 Products</a></li>
+                </ul>
+            </div>
+
+            <!-- sort by price -->
+            <style>
+                    /* Style the dropdown container */
+                    .pdropdown {
+                    position: relative;
+                    display: inline-block;
+                    }
+
+                    /* Style the button that toggles the dropdown */
+                    .pdropbtn {
+                    background-color: #3498db;
+                    color: white;
+                    padding: 10px;
+                    border: none;
+                    cursor: pointer;
+                    }
+
+                    /* Style the dropdown content (hidden by default) */
+                    .pdropdown-content {
+                    display: none;
+                    position: absolute;
+                    background-color: #f9f9f9;
+                    min-width: 160px;
+                    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+                    z-index: 1;
+                    }
+
+                    /* Style the dropdown content links */
+                    .pdropdown-content a {
+                    color: black;
+                    padding: 12px 16px;
+                    text-decoration: none;
+                    display: block;
+                    }
+
+                    /* Change color of dropdown content links on hover */
+                    .pdropdown-content a:hover {
+                    background-color: #ddd;
+                    }
+
+                    /* Show the dropdown content when hovering over the container */
+                    .pdropdown:hover .pdropdown-content {
+                    display: block;
+                    }
+
+            </style>
+
+            <div class="pdropdown">
+                <button class="pdropbtn" >Sort By: {{$orderBy}}</button>
+                <ul class="pdropdown-content">
+                    <li><a href="#" wire:click.prevent="changeOrderBy('Default Sorting')">Default Sorting</a></li>
+                    <li><a href="#" wire:click.prevent="changeOrderBy('Price: Low to High')">Price: Low to High</a></li>
+                    <li><a href="#" wire:click.prevent="changeOrderBy('Price: High to Low')">Price: High to Low</a></li>
+                    <li><a href="#" wire:click.prevent="changeOrderBy('Latest')">Latest</a></li>
+                </ul>
+            </div>
+          
+            <div class="shop-container">
             
               <div class="product-cards">
                 @foreach($products as $product)
