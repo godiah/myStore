0fa0d1d38940e169691c996e383b26be4c36c66f
lewys-miugo <lewiswambugu01@gmail.com>
2023-08-27 21:36:54 +0300

    Adding the route to wishlist

--- resources/views/livewire/wishlist-icon-component.blade.php
+++ resources/views/livewire/wishlist-icon-component.blade.php
@@ -1,5 +1,5 @@
 <li>
-    <a class="dropdown-item" href="wishlist.html">
+    <a class="dropdown-item" href="{{route('shop.wishlist')}}">
         <i class="fa-solid fa-gift"></i>
         Saved Items 
         @if (Cart::instance('wishlist')->count()>0)
