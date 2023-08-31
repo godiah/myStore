67c0b3eccf68f62de38790790765567d3e8e92c0
lewys-miugo <lewiswambugu01@gmail.com>
2023-08-31 11:02:15 +0300

    Adding rship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $table = "categories";
+
+    public function subCategories()
+    {
+        return $this->hasMany(SubCategory::class,'sub_category_id');
+    }
+
+    public function superCategories()
+    {
+        $this->belongsTo(SuperCategory::class);
+    }
 }
