1ac044b1240ce21e3344eea38183c4ab584c4857
lewys-miugo <lewiswambugu01@gmail.com>
2023-09-02 01:45:14 +0300

    building the add product form

--- resources/views/livewire/admin/admin-add-products-component.blade.php
+++ resources/views/livewire/admin/admin-add-products-component.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
         <div class="col-md-3"></div>
         <div class="col-md-6">
-            <form action="" method="post"  class="sign-up-form mt-5 mb-5" enctype="multipart/form-data">
+            <form wire:submit.prevent="addProduct" class="sign-up-form mt-5 mb-5" enctype="multipart/form-data">
                 <h2 class="title">New Product Form</h2>
                 <!-- Product Name -->
                 <div class="input-field">
@@ -30,17 +30,18 @@
                         <p styles="color:red;" class="">{{$message}}</p>
                     @enderror
                 </div>
+
                 <!-- Product Super Category -->
                 <div class="input-field">
                     <!-- <label for=""></label> -->
-                    <span class="material-icons-outlined">category</span>            
-                    <select name="category" id="">
-                        <option value="">Select Category</option>
-                        <option value="">Category 1</option>
-                        <option value="">Category 2</option>
-                        <option value="">Category 3</option>
+                    <span class="material-icons-outlined">Super Category</span>            
+                    <select name="sup_category_id" id="" wire:model="sup_category_id" wire:change="changeCategory">
+                        <option value="">Select Super Category</option>
+                        @foreach ($supcategories as $supcategory)
+                            <option value="{{$supcategory->id}}">{{$supcategory->name}}</option>
+                        @endforeach
                     </select>
-                    @error('category')
+                    @error('sup_category_id')
                         <p styles="color:red;" class="">{{$message}}</p>
                     @enderror
                 </div>
@@ -49,28 +50,28 @@
                 <div class="input-field">
                     <!-- <label for=""></label> -->
                     <span class="material-icons-outlined">category</span>            
-                    <select name="category" id="">
+                    <select name="category_id" id="" wire:model="category_id" wire:change="changeSubcategory">
                         <option value="">Select Category</option>
-                        <option value="">Category 1</option>
-                        <option value="">Category 2</option>
-                        <option value="">Category 3</option>
+                        @foreach ($categories as $category)
+                            <option value="{{$category->id}}">{{$category->name}}</option>
+                        @endforeach
                     </select>
-                    @error('category')
+                    @error('category_id')
                         <p styles="color:red;" class="">{{$message}}</p>
                     @enderror
                 </div>
 
-                <!-- Product Sub Category -->
+                <!-- Product sub category -->
                 <div class="input-field">
                     <!-- <label for=""></label> -->
-                    <span class="material-icons-outlined">category</span>            
-                    <select name="category" id="">
+                    <span class="material-icons-outlined">sub category</span>            
+                    <select name="sub_category_id" id="" wire:model="sub_category_id">
                         <option value="">Select Category</option>
-                        <option value="">Category 1</option>
-                        <option value="">Category 2</option>
-                        <option value="">Category 3</option>
+                        @foreach ($subcategories as $subcategory)
+                            <option value="{{$subcategory->id}}">{{$subcategory->name}}</option>  
+                        @endforeach
                     </select>
-                    @error('category')
+                    @error('sub_category_id')
                         <p styles="color:red;" class="">{{$message}}</p>
                     @enderror
                 </div>
@@ -78,48 +79,50 @@
                 <!-- Product Instock -->
                 <div class="input-field">
                     <!-- <label for=""></label> -->
-                    <span class="material-icons-outlined">Instock/Out of stock</span>            
-                    <select name="category" id="">
+                    <span class="material-icons-outlined">Stock Status</span>            
+                    <select name="stock_status" id="" wire:model="stock_status">
                         <option value="">Select Category</option>
-                        <option value="">Instock</option>
-                        <option value="">Out of Stock</option>
+                        <option value="in stock">Instock</option>
+                        <option value="out of stock">Out of Stock</option>
                         <!-- <option value="">Category 3</option> -->
                     </select>
-                    @error('category')
+                    @error('stock_status')
                         <p styles="color:red;" class="">{{$message}}</p>
                     @enderror
                 </div>
+
                 <!-- Product Keyword -->
                 <!-- <div class="input-field">
                     <label for=""></label>
                     <span class="material-icons-outlined"><i class="fa-solid fa-key"></i></span>
                     <input type="text" name="" id="" placeholder="Keywords" required autocomplete="off">
                 </div> -->
+
                 <!-- Product Images -->
                 <!-- Image 1 -->
                 <div class="input-field">
                     <!-- <label for=""></label> -->
                     <span class="material-icons-outlined"><i class="fa-solid fa-image"></i></span>
-                    <input type="file" name="" id="" placeholder="Product Name" required autocomplete="off">
+                    <input type="file" name="" id="" placeholder="Product Name" required autocomplete="off" wire:model="image">
                 </div>
                 <!-- Image 2 -->
                 <div class="input-field">
                     <!-- <label for=""></label> -->
                     <span class="material-icons-outlined"><i class="fa-solid fa-image"></i></span>
-                    <input type="file" name="" id="" placeholder="Product Name" required autocomplete="off">
+                    <input type="file" name="" id="" placeholder="Product Name" required autocomplete="off" wire:model="image2">
                 </div>
                 <div class="d-flex">
                     <!-- Product Quantity -->
                 <div class="input-field">
                     <!-- <label for=""></label> -->
                     <span class="material-icons-outlined"><i class="fa-solid fa-basket-shopping"></i></span>
-                    <input type="number" name="" id="" placeholder="Quantity" required autocomplete="off">
+                    <input type="number" name="quantity" id="" placeholder="Quantity" required autocomplete="off" wire:model="quantity">
                 </div>
                 <!-- Product Price -->
                 <div class="input-field">
                     <!-- <label for=""></label> -->
                     <span class="material-icons-outlined"><i class="fa-solid fa-tag"></i></span>
-                    <input type="number" name="" id="" placeholder="Price" required autocomplete="off" wire:model="regular_price">
+                    <input type="number" name="regular_price" id="" placeholder="Price" required autocomplete="off" wire:model="regular_price">
                 </div>
                 </div>                         
                 <!-- Submit Button -->
