6e9e25ee9caa5b3b19c133b73b4530e2e84e42a0
lewys-miugo <lewiswambugu01@gmail.com>
2023-09-04 19:00:47 +0300

    building Product edit

--- app/Http/Livewire/Admin/AdminEditProductComponent.php
+++ app/Http/Livewire/Admin/AdminEditProductComponent.php
@@ -3,11 +3,165 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\Category;
+use App\Models\SuperCategory;
+use App\Models\SubCategory;
+use App\Models\Product;
+use App\Models\ProductImages;
+use Carbon\Carbon;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Str;
 
 class AdminEditProductComponent extends Component
 {
+    use WithFileUploads;
+
+    public $name;
+    public $slug;
+    public $short_description;
+
+    public $description;
+    public $regular_price;
+
+    public $stock_status ='instock';
+    public $quantity;
+
+    public $image;
+    public $image2;
+    public $image3;
+    public $image4;
+
+    public $newimage;
+    public $newimage2;
+    public $newimage3;
+    public $newimage4;
+
+    public $unique_id;
+    public $product_id;
+
+    public $category_id;
+    public $sup_category_id;
+    public $sub_category_id;
+    
+    public $cat_id;
+    public $subcat_id;
+
+    public function mount($product_id)
+    {
+        $product = Product::find($product_id);
+        $this->product_id = $product->id;
+        $this->name = $product->name;
+        $this->slug = $product->slug;
+        $this->short_description      = $product->short_description;
+        $this->description      = $product->description;
+        $this->regular_price    = $product->regular_price;
+        $this->sup_category_id  = $product->sup_category_id;
+        $this->category_id      = $product->category_id;
+        $this->sub_category_id  = $product->sub_category_id;
+        $this->stock_status     = $product->stock_status;
+        $this->quantity         = $product->quantity;
+
+        // images
+
+        $this->image=$product->image;
+        $this->image2=$product->image2;
+        $this->image3=$product->image3;
+        $this->image4=$product->image4;
+        $this->image5=$product->image5;
+        $this->image6=$product->image6;
+        $this->image7=$product->image7;
+    }
+
+    public function deleteImage($id)
+    {
+        $image=ProductImages::where('id', $id)->first();
+        $image->delete();
+        session()->flash('message', 'Image has been deleted');
+
+    }
+
+    public function generateSlug()
+    {
+        $this->slug=Str::slug($this->name);
+    }
+
+    public function updateProduct()
+    {
+        $this->validate([
+            'name'=>'required',
+            // 'slug'=>'required',
+            // 'regular_price'=>'required',
+            'sup_category_id'=>'required',
+        ]);
+
+        $product = Product::find($this->product_id);
+        $product->name = $this->name;
+        $product->slug = $this->slug;
+        $product->short_description = $this->short_description;
+        $product->description = $this->description;
+        $product->regular_price = $this->regular_price;
+
+        $product->sup_category_id = $this->sup_category_id;
+        $product->category_id = $this->category_id;
+        $product->sub_category_id = $this->sub_category_id;
+
+        $productName = $product->slug;
+
+        if ($this->newimage) {
+            unlink('images/products/'.$product->image);
+        $productName = $product->slug;
+            $imageName = $productName.'_'.Carbon::now()->timestamp.'.'.$this->newimage->extension();
+            $this->newimage->storeAs('products', $imageName);
+            $product->image = $imageName;
+        }
+
+        if ($this->newimage2) {
+            unlink('images/products/'.$product->image2);
+            $imageName = $productName.'_'.Carbon::now()->timestamp.'.'.Str::random(6).$this->newimage2->extension();
+            $this->newimage2->storeAs('products', $imageName);
+            $product->image2 = $imageName;
+        }
+
+        if ($this->newimage3) {
+            if ($this->image3) {
+                unlink('images/products/'.$product->image3);
+            }
+            $imageName = $productName.'_'.Carbon::now()->timestamp.'.'.Str::random(6).$this->newimage3->extension();
+            $this->newimage3->storeAs('products', $imageName);
+            $product->image3 = $imageName;
+        }
+
+        if ($this->newimage4) {
+            if ($this->image4) {
+                unlink('images/products/'.$product->image4);
+            }
+            $imageName = $productName.'_'.Carbon::now()->timestamp.'.'.Str::random(6).$this->newimage4->extension();
+            $this->newimage4->storeAs('products', $imageName);
+            $product->image4 = $imageName;
+        }
+
+        $product->save();
+        session()->flash('message', 'Product details have been Updated!');
+    }
+
+    public function changeCategory()
+    {
+        $this->category_id=0;
+    }
+
+    public function changeSubcategory()
+    {
+        $this->sub_category_id=0;
+    }
+
+
     public function render()
     {
-        return view('livewire.admin.admin-edit-product-component');
+        $supcategories=SuperCategory::orderBy('name','DESC')->get();
+        $categories=Category::where('sup_category_id',$this->sup_category_id)->get();
+        $subcategories=SubCategory::where('category_id',$this->category_id)->get();
+
+
+        return view('livewire.admin.admin-edit-product-component',['supcategories'=>$supcategories,'categories'=>$categories,'subcategories'=>$subcategories]);
     }
 }
--- resources/views/livewire/admin/admin-edit-product-component.blade.php
+++ resources/views/livewire/admin/admin-edit-product-component.blade.php
@@ -1,3 +1,183 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <style>
+        /* ::-webkit-scrollbar{
+        display: none;
+    }  */
+    body{
+        overflow-x: hidden;
+    }
+    </style>
+    <!-- Edit Product Form -->
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+        @if(Session::has('message'))
+            <div class="">
+                <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+                </svg> -->
+                <span class="">Success! | {{Session::get('message')}}</span>
+            </div>
+        @endif
+            <form action="" method="post"  class="sign-up-form mt-5 mb-5" enctype="multipart/form-data" wire:submit.prevent="updateProduct" >
+                <h2 class="title">Edit Product Form</h2>
+                <!-- Product Name -->
+                <div class="input-field">
+                    <span class="material-icons-outlined"><i class="fa-solid fa-layer-group"></i></span>
+                    <input type="text" name="name" id="" placeholder="Name" required autocomplete="off" wire:model="name" wire:keyup="generateSlug()">
+                    @error('name')
+                        <p styles="color:red;" class="">{{$message}}</p>
+                    @enderror
+                </div>
+                 <!-- Product Slug -->
+                <div class="input-field">
+                    <!-- <label for=""></label> -->
+                    <span class="material-icons-outlined"><i class="fa-solid fa-layer-group"></i></span>
+                    <input type="text" name="slug" id="slug" placeholder="Slug" required autocomplete="off" wire:model="slug">
+                    @error('slug')
+                        <p styles="color:red;" class="">{{$message}}</p>
+                    @enderror
+                </div>
+                <div class="input-field">
+                    <!-- <label for=""></label> -->
+                    <span class="material-icons-outlined"><i class="fa-solid fa-note-sticky"></i></span>
+                    <input type="text" name="short_description" id="" placeholder="Short Description" required autocomplete="off" wire:model="short_description">
+                    @error('short_description')
+                        <p styles="color:red;" class="">{{$message}}</p>
+                    @enderror
+                </div>
+
+                <!-- Product Description -->
+                <div class="input-field">
+                    <span class="material-icons-outlined"><i class="fa-solid fa-note-sticky"></i></span>
+                    <input type="text" name="description" id="" placeholder="Description" required autocomplete="off" wire:model="description">
+                    @error('description')
+                        <p styles="color:red;" class="">{{$message}}</p>
+                    @enderror
+                </div>
+                <!-- Product Super Category -->
+                <div class="input-field">
+                    <!-- <label for=""></label> -->
+                    <span class="material-icons-outlined">Super Category</span>            
+                    <select name="sup_category_id" id="" wire:model="sup_category_id" wire:change="changeCategory">
+                        <option value="">Select Super Category</option>
+                        @foreach ($supcategories as $supcategory)
+                            <option value="{{$supcategory->id}}">{{$supcategory->name}}</option>
+                        @endforeach
+                    </select>
+                    @error('sup_category_id')
+                        <p styles="color:red;" class="">{{$message}}</p>
+                    @enderror
+                </div>
+
+                <!-- Product Category -->
+                <div class="input-field">
+                    <!-- <label for=""></label> -->
+                    <span class="material-icons-outlined">category</span>            
+                    <select name="category_id" id="" wire:model="category_id" wire:change="changeSubcategory">
+                        <option value="">Select Category</option>
+                        @foreach ($categories as $category)
+                            <option value="{{$category->id}}">{{$category->name}}</option>
+                        @endforeach
+                    </select>
+                    @error('category_id')
+                        <p styles="color:red;" class="">{{$message}}</p>
+                    @enderror
+                </div>
+
+                <!-- Product sub category -->
+                <div class="input-field">
+                    <!-- <label for=""></label> -->
+                    <span class="material-icons-outlined">sub category</span>            
+                    <select name="sub_category_id" id="" wire:model="sub_category_id">
+                        <option value="">Select Category</option>
+                        @foreach ($subcategories as $subcategory)
+                            <option value="{{$subcategory->id}}">{{$subcategory->name}}</option>  
+                        @endforeach
+                    </select>
+                    @error('sub_category_id')
+                        <p styles="color:red;" class="">{{$message}}</p>
+                    @enderror
+                </div>
+                <!-- Product Keyword -->
+                <!-- <div class="input-field">
+                    <span class="material-icons-outlined"><i class="fa-solid fa-key"></i></span>
+                    <input type="text" name="" id="" placeholder="Keywords" required autocomplete="off">
+                </div> -->
+                <!-- Product Images -->
+                <div class="input-field">
+                    <span class="material-icons-outlined"><i class="fa-solid fa-image"></i></span>
+                    <input type="file" name="image" id="" placeholder="Product Name"  autocomplete="off" wire:model="newimage">
+                    @if($newimage)
+                        <img src="{{$newimage->temporaryUrl()}}" class="" style="width:30px" alt="">
+                    @else
+                        <img src="{{asset('images/products')}}/{{$image}}" class="" style="width:30px" alt="">    
+                    @endif    
+                    @error('newimage')
+                        <p style="text-color:red;">{{$message}}</p>
+                    @enderror
+                </div>
+
+                <!-- Product Image2 -->
+                <div class="input-field">
+                    <span class="material-icons-outlined"><i class="fa-solid fa-image"></i></span>
+                    <input type="file" name="image2" id="" placeholder="Product Name"  autocomplete="off" wire:model="newimage2">
+                    @if($newimage2)
+                        <img src="{{$newimage2->temporaryUrl()}}" class="" style="width:30px" alt="">
+                    @else
+                        <img src="{{asset('images/products')}}/{{$image}}" class="" style="width:30px" alt="">    
+                    @endif    
+                    @error('newimage2')
+                        <p class="" style="text-color:red;">{{$message}}</p>
+                    @enderror
+                </div>
+
+                <!-- Product Image3 -->
+                <div class="input-field">
+                    <span class="material-icons-outlined"><i class="fa-solid fa-image"></i></span>
+                    <input type="file" name="image3" id="" placeholder="Product Name"  autocomplete="off" wire:model="newimage3">
+                    @if($newimage3)
+                        <img src="{{$newimage3->temporaryUrl()}}" class="" style="width:30px" alt="">
+                    @else
+                        <img src="{{asset('images/products')}}/{{$image}}" class="" style="width:30px" alt="">    
+                    @endif    
+                    @error('newimage3')
+                        <p class="" style="text-color:red;">{{$message}}</p>
+                    @enderror
+                </div>
+
+                <!-- Product Image4 -->
+                <div class="input-field">
+                    <span class="material-icons-outlined"><i class="fa-solid fa-image"></i></span>
+                    <input type="file" name="image4" id="" placeholder="Product Name"  autocomplete="off" wire:model="newimage4">
+                    @if($newimage4)
+                        <img src="{{$newimage4->temporaryUrl()}}" class="" style="width:30px" alt="">
+                    @else
+                        <img src="{{asset('images/products')}}/{{$image}}" class="" style="width:30px" alt="">    
+                    @endif    
+                    @error('newimage4')
+                        <p class="" style="text-color:red;">{{$message}}</p>
+                    @enderror
+                </div>
+                
+                <div class="d-flex">
+                    <!-- Product Quantity -->
+                    <div class="input-field">
+                        <!-- <label for=""></label> -->
+                        <span class="material-icons-outlined"><i class="fa-solid fa-basket-shopping"></i></span>
+                        <input type="number" name="quantity" id="" placeholder="Quantity" required autocomplete="off" wire:model="quantity">
+                    </div>
+                    <!-- Product Price -->
+                    <div class="input-field">
+                        <!-- <label for=""></label> -->
+                        <span class="material-icons-outlined"><i class="fa-solid fa-tag"></i></span>
+                        <input type="number" name="regular_price" id="" placeholder="Price" required autocomplete="off" wire:model="regular_price">
+                    </div>
+                </div>                        
+                <!-- Submit Button -->
+                <input type="submit" name="" id="" class="btn solid" value="Edit Product">
+            </form>            
+        </div>
+        <div class="col-md-3"></div>
+    </div>
 </div>
