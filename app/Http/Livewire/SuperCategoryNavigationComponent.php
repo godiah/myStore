bc069beb592bd48716b06eef8fda40a6e2625142
lewys-miugo <lewiswambugu01@gmail.com>
2023-09-05 11:17:16 +0300

    Building the main nav component

---
+++ app/Http/Livewire/SuperCategoryNavigationComponent.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Category;
+use App\Models\SuperCategory;
+use App\Models\SubCategory;
+
+class SuperCategoryNavigationComponent extends Component
+{
+    public $supcategories;
+    public $selectedSupcategory;
+    public $selectedCategory;
+
+    public function mount()
+    {
+        $this->supcategories = SuperCategory::with('categories.subcategories')->get();
+    }
+
+    // public function showCategories($supcategoryId, $categoryId)
+    // {
+    //     $this->selectedSupcategory = $supcategoryId;
+    //     $this->selectedCategory = $categoryId;
+    // }
+    public function showCategories($supcategoryId, $categoryId)
+    {
+        $this->selectedSupcategory = $supcategoryId;
+        $this->selectedCategory = $categoryId;
+    }
+
+
+    public function render()
+    {
+        return view('livewire.super-category-navigation-component');
+    }
+}
---
+++ resources/views/livewire/super-category-navigation-component.blade.php
@@ -0,0 +1,73 @@
+<div>
+    {{-- Be like water. --}}
+    <style>
+      .hidden{
+          display:none;
+      }
+      .visible{
+        display: flex; /* Use flexbox to create a row layout */
+        list-style: none; /* Remove default list bullets/numbers */
+        padding: 0; /* Remove default padding */
+      }
+      .cat_gory{
+        color:red;
+      }
+      .di_sup_cat{
+        margin:8px;
+      }
+      .di_cat{
+        /* margin:4px; */
+      }
+      .list-container {
+        display: flex; /* Use flexbox to create a row layout */
+        list-style: none; /* Remove default list bullets/numbers */
+        padding: 0; /* Remove default padding */
+      }
+      .list-item {
+          margin-right: 10px; /* Add space between list items */
+          /* Additional styling as needed */
+      }
+    </style>
+    <div>
+      <ul class="list-container">
+          @foreach ($supcategories as $supcategory)
+              <li>
+                  <div class="di_sup_cat">
+                      <a href="#" wire:mouseover="showCategories({{ $supcategory->id }}, null)"
+                        wire:mouseout="showCategories(null, null)">
+                          {{ $supcategory->name }} <i class="fa-solid fa-caret-down"></i>
+                      </a>
+                  
+
+                    <ul class="{{ $selectedSupcategory == $supcategory->id ? 'visible' : 'hidden' }}">
+                        @foreach ($supcategory->categories as $category)
+                            <li class="list-item">
+                                <!-- wire:mouseover="showCategories({{ $supcategory->id }}, {{ $category->id }})"
+                                      wire:mouseout="showCategories({{ $supcategory->id }}, null)" -->
+                                <div class="di_cat " wire:mouseover="showCategories({{ $supcategory->id }}, {{ $category->id }})"
+                                      wire:mouseout="showCategories({{ $supcategory->id }}, null)">
+                                    <a href="#" 
+                                      class="cat_gory">
+                                        {{ $category->name }} <i class="fa-solid fa-caret-down"></i>
+                                    </a>
+                                
+                                    <!-- class="{{ $selectedCategory == $category->id ? 'visible' : 'hidden' }}" -->
+                                  <ul >
+                                      @foreach ($category->subcategories as $subcategory)
+                                          <li>
+                                              <a href="#" class="mx-4">{{ $subcategory->name }}</a>
+                                          </li>
+                                      @endforeach
+                                  </ul>
+                                </div>
+                            </li>
+                        @endforeach
+                    </ul>
+                  </div>
+              </li>
+          @endforeach
+      </ul>
+    </div>
+    
+
+</div>
