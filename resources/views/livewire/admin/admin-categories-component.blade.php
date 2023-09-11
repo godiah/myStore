0b1f8412f8e4d3f1fd6a6737735c126e02f97c05
lewys-miugo <lewiswambugu01@gmail.com>
2023-09-11 15:38:12 +0300

    Adding the delete functions and displaying all categories subcategories and super categories

--- resources/views/livewire/admin/admin-categories-component.blade.php
+++ resources/views/livewire/admin/admin-categories-component.blade.php
@@ -2,22 +2,64 @@
     
     <!-- All Categories -->
     <div class="container" id="admin-prod-cont">
-        <h3 class="text-center">All Categories:</h3>
+        <h3 class="text-center">All Super Categories:</h3>
+        <a class="text-center" href="{{route('admin.category.add')}}">Add New Super Category / Category / SubCategory</a>
+
+            @foreach ($supcategories as $supcategory)
+                <div class="admin-prod-item" id="supcategories">
+                    <!-- <div class="admin-prod-item-image">
+                        <a href="#">
+                            <img src="../images/product1.jpg" alt="Product Image">
+                        </a>
+                    </div> -->
+                    <div class="admin-prod-item-details">              
+                    <h5>{{$supcategory->name}}</h5>              
+                    </div>
+                    <div class="admin-prod-item-buttons">
+                    <a href="{{route('admin.category.edit',['category_id'=>$supcategory->id])}}" class="buy-now"><i class="fa-solid fa-pen-to-square"></i>Edit</a>
+                    <a href="#" wire:click.prevent="deleteSupCategory({{ $supcategory->id }})" class="remove-product"><i class="fa-solid fa-trash"></i> Remove</a>
+                    </div>
+                </div>
+            @endforeach
+
+            <h3 class="text-center">All Categories:</h3>
+            <a href="{{route('admin.category.add')}}"></a>
+
             @foreach ($categories as $category)
-            <div class="admin-prod-item">
-                <!-- <div class="admin-prod-item-image">
-                <a href="#">
-                    <img src="../images/product1.jpg" alt="Product Image">
-                </a>
-                </div> -->
-                <div class="admin-prod-item-details">              
-                <h5>{{$category->name}}</h5>              
+                <div class="admin-prod-item" id="categories">
+                    <!-- <div class="admin-prod-item-image">
+                    <a href="#">
+                        <img src="../images/product1.jpg" alt="Product Image">
+                    </a>
+                    </div> -->
+                    <div class="admin-prod-item-details">              
+                    <h5>{{$category->name}}</h5>              
+                    </div>
+                    <div class="admin-prod-item-buttons">
+                    <a href="{{route('admin.category.edit',['category_id'=>$category->id])}}" class="buy-now"><i class="fa-solid fa-pen-to-square"></i>Edit</a>
+                    <a href="#" wire:click.prevent="deleteCategory({{ $category->id }})" class="remove-product"><i class="fa-solid fa-trash"></i> Remove</a>
+                    </div>
                 </div>
-                <div class="admin-prod-item-buttons">
-                <a href="{{route('admin.category.edit',['category_id'=>$category->id])}}" class="buy-now"><i class="fa-solid fa-pen-to-square"></i>Edit</a>
-                <a href="#" wire:click.prevent="deleteCategory({{ $category->id }})" class="remove-product"><i class="fa-solid fa-trash"></i> Remove</a>
+            @endforeach
+
+            <h3 class="text-center">All Sub Categories:</h3>
+            <a href="{{route('admin.category.add')}}"></a>
+
+            @foreach ($subcategories as $subcategory)
+                <div class="admin-prod-item" id="subcategories">
+                    <!-- <div class="admin-prod-item-image">
+                    <a href="#">
+                        <img src="../images/product1.jpg" alt="Product Image">
+                    </a>
+                    </div> -->
+                    <div class="admin-prod-item-details">              
+                    <h5>{{$subcategory->name}}</h5>              
+                    </div>
+                    <div class="admin-prod-item-buttons">
+                    <a href="{{route('admin.category.edit',['category_id'=>$subcategory->id])}}" class="buy-now"><i class="fa-solid fa-pen-to-square"></i>Edit</a>
+                    <a href="#" wire:click.prevent="deleteSubCategory({{ $subcategory->id }})" class="remove-product"><i class="fa-solid fa-trash"></i> Remove</a>
+                    </div>
                 </div>
-            </div>
             @endforeach
             <!-- <div class="admin-prod-item">
                 <div class="admin-prod-item-image">
@@ -47,6 +89,6 @@
                 <a href="#" class="remove-product"><i class="fa-solid fa-trash"></i> Remove</a>
                 </div>
             </div> -->
-            {{$categories->links()}}
+            {{-- $categories->links() --}}
     </div>
 </div>
