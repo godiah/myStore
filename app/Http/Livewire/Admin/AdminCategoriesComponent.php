e1e0a1e95fbae1183a24c4ccb02fd886c060a414
lewys-miugo <lewiswambugu01@gmail.com>
2023-09-11 15:37:12 +0300

    Building the delete functions

--- app/Http/Livewire/Admin/AdminCategoriesComponent.php
+++ app/Http/Livewire/Admin/AdminCategoriesComponent.php
@@ -3,6 +3,8 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\Category;
+use App\Models\SuperCategory;
+use App\Models\SubCategory;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -11,6 +13,8 @@ class AdminCategoriesComponent extends Component
     use WithPagination;
 
     public $category_id;
+    public $sup_category_id;
+    public $sub_category_id;
 
     public function deleteCategory($categoryId)
     {
@@ -21,10 +25,31 @@ class AdminCategoriesComponent extends Component
         session()->flash('message','Category has been deleted succesfully');
     }
 
+    public function deleteSupCategory($supcategoryId)
+    {
+        $this->supcategory_id = $supcategoryId;
+
+        $supercategory= SuperCategory::find($this->supcategory_id);
+        $supercategory->delete();
+        session()->flash('message','Super Category has been deleted succesfully');
+    }
+
+    public function deleteSubCategory($subcategoryId)
+    {
+        $this->subcategory_id = $subcategoryId;
+
+        $subcategory= SubCategory::find($this->subcategory_id);
+        $subcategory->delete();
+        session()->flash('message','Sub Category has been deleted succesfully');
+    }
+
     public function render()
     {
-        $categories=Category::orderBy('name','ASC')->paginate(12);
+        $categories=Category::orderBy('name','ASC')->get();
+        $supcategories=SuperCategory::orderBy('name','ASC')->get();
+        $subcategories=SubCategory::orderBy('name','ASC')->get();
 
-        return view('livewire.admin.admin-categories-component',['categories'=>$categories]);
+
+        return view('livewire.admin.admin-categories-component',['categories'=>$categories,'supcategories'=>$supcategories,'subcategories'=>$subcategories]);
     }
 }
