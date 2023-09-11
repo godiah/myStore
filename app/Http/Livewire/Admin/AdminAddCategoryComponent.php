52fb4c3cc76dd7a88ba1aa2c39aea528dcddb1fa
lewys-miugo <lewiswambugu01@gmail.com>
2023-09-11 15:38:47 +0300

    building functionality you can add supercategory/subcategory/category

--- app/Http/Livewire/Admin/AdminAddCategoryComponent.php
+++ app/Http/Livewire/Admin/AdminAddCategoryComponent.php
@@ -4,12 +4,17 @@ namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
 use App\Models\Category;
+use App\Models\SuperCategory;
+use App\Models\SubCategory;
 use Illuminate\Support\Str;
 
 class AdminAddCategoryComponent extends Component
 {
     public $name;
     public $slug;
+
+    public $sup_category_id;
+    public $category_id;
 
     public function generateSlug()
     {
@@ -30,16 +35,44 @@ class AdminAddCategoryComponent extends Component
             'name'=>'required',
             'slug'=>'required' 
         ]);
+
+        if ($this->sup_category_id) {
+            $category= new Category();
+            $category->name = $this->name;
+            $category->sup_category_id=$this->sup_category_id;
+            $category->slug =$this->slug;
+            $category->save();
+            session()->flash('message','New category has been created successfully!');
+            return redirect()->route('admin.categories');
+        }
+
+        elseif ($this->category_id) {
+            # code...
+            $sub_category= new SubCategory();
+            $sub_category->name = $this->name;
+            $sub_category->slug =$this->slug;
+            $sub_category->category_id=$this->category_id;
+            $sub_category->save();
+            session()->flash('message','New Sub category has been created successfully!');
+            return redirect()->route('admin.categories');
+        }
+        else{
+            $sup_category= new SuperCategory();
+            $sup_category->name = $this->name;
+            $sup_category->slug =$this->slug;
+            $sup_category->save();
+            session()->flash('message','New Super category has been created successfully!');
+            return redirect()->route('admin.categories');
+        }
         
-        $category= new Category();
-        $category->name = $this->name;
-        $category->slug =$this->slug;
-        $category->save();
-        session()->flash('message','New category has been created successfully!');
-        return redirect()->route('admin.categories');
+        
     }
     public function render()
     {
-        return view('livewire.admin.admin-add-category-component');
+        $supcategories=SuperCategory::orderBy('name','DESC')->get();
+        $categories=Category::where('sup_category_id',$this->sup_category_id)->get();
+        $subcategories=SubCategory::where('category_id',$this->category_id)->get();
+
+        return view('livewire.admin.admin-add-category-component',['supcategories'=>$supcategories,'categories'=>$categories,'subcategories'=>$subcategories]);
     }
 }
--- resources/views/livewire/admin/admin-add-category-component.blade.php
+++ resources/views/livewire/admin/admin-add-category-component.blade.php
@@ -16,19 +16,39 @@
                 <!-- Category Name -->
                 <div class="input-field">
                     <span class="material-icons-outlined">style</span>
-                    <input type="text" name="name" id="name" placeholder="Category Name" required autocomplete="off" wire:model="name" wire:keyup="generateSlug()">
+                    <input type="text" name="name" id="name" placeholder=" Name" required autocomplete="off" wire:model="name" wire:keyup="generateSlug()">
                     @error('name')
                         <p styles="color:red;" class="">{{$message}}</p>
                     @enderror
                 </div>     
                <!-- Category slug -->
-               <div class="input-field">
+                <div class="input-field">
                     <span class="material-icons-outlined">style</span>
-                    <input type="text" name="slug" id="slug"  required autocomplete="off" wire:model="slug">
+                    <input type="text" name="slug" id="slug" placeholder="Slug"  required autocomplete="off" wire:model="slug">
                     @error('slug')
                         <p styles="color:red;"  class="">{{$message}}</p>
                     @enderror
                 </div>
+                <!--Super Category Name -->
+                <div class="input-field">
+                    <span class="material-icons-outlined"><i class="fa-brands fa-slack"></i></span>            
+                    <select name="sup_category_id" id="" placeholder="Super Category" wire:model="sup_category_id">
+                        <option value="">Select Super Category</option>
+                        @foreach ($supcategories as $supcategory)
+                            <option value="{{$supcategory->id}}">{{$supcategory->name}}</option>
+                        @endforeach
+                    </select>
+                </div> 
+                <!--Category Name -->
+                <div class="input-field">
+                    <span class="material-icons-outlined">category</span>            
+                    <select name="category_id" id="" placeholder="Category" wire:model="category_id">
+                        <option value="">Select Category</option>
+                        @foreach ($categories as $category)
+                            <option value="{{$category->id}}">{{$category->name}}</option>
+                        @endforeach
+                    </select>
+                </div> 
                 <!-- Submit Button -->
                 <input type="submit" name="" id="" class="btn solid" value="Add Category">
             </form>
