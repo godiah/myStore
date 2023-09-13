cdd097fd4879ef9e2e4c04cd5b9e9bd040e02b5d
lewys-miugo <lewiswambugu01@gmail.com>
2023-09-13 19:44:29 +0300

    Building the shippings table/model

---
+++ database/migrations/2023_09_13_160238_create_shippings_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shippings', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('order_id')->unsigned();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('mobile');
+            $table->string('email');
+            $table->string('line1')->nullable();
+            $table->string('line2')->nullable();
+            $table->string('country')->default('Kenya');
+            $table->string('county');
+            $table->string('subcounty')->nullable();
+            $table->string('town');
+            $table->string('specificarea');
+            $table->timestamps();
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shippings');
+    }
+};
