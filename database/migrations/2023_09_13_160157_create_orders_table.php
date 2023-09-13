8b67e4f428974a011ebbca6a969d4aef587ba786
lewys-miugo <lewiswambugu01@gmail.com>
2023-09-13 19:42:14 +0300

    Building the orders table/model

---
+++ database/migrations/2023_09_13_160157_create_orders_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->decimal('subtotal',8,2,'.', ',')->nullable();
+            // $table->decimal('discount')->default (0);
+            $table->decimal('tax')->nullable();
+            $table->decimal('total',12,2)->nullable();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('mobile');
+            $table->string('email');
+            $table->string('line1');
+            $table->string('line2')->nullable();
+            $table->string('country')->default('Kenya');
+            $table->string('county');
+            $table->string('subcounty');
+            $table->string('town');
+            $table->string('specificarea');
+            $table->enum('status', ['ordered', 'delivered', 'canceled'])->default('ordered');
+            $table->boolean ('is_shipping_different')->default(false);
+            $table->timestamps();
+            $table->foreign('user_id')->references ('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
